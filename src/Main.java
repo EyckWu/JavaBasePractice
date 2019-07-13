350e712a00c64ecae49296334f363cb591e6d0d4
WuGuiXin <EyckWu@gmail.com>
2019-07-13 08:57:02 +0800

    replace

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        String selection = "number LIKE ?";
+        String selection1 = "NUMBER LIKE ?";
+        selection = selection.replaceAll("(?i)number", "shortDialNumber");
+        selection1 = selection1.replaceAll("(?i)number", "shortDialNumber");
+        System.out.println("selection = " + selection);
+        System.out.println("selection1 = " + selection1);
     }
 }
